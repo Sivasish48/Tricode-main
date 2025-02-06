bf7466df822b077f2986ea995a682c5ce3befa05
Sivasish Praharaj <sivasish48@gmail.com>
2025-02-07 01:17:35 +0530

    readjusting server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Match your frontend URL
+    origin: "http://localhost:5174", // Match your frontend URL
     credentials: true, // Allow cookies to be sent
   })
 );
